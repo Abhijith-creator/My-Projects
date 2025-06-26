3d98bfd72f050f14767248033fdd3e5229a0f47f
Abhijith Anbalagan <abhijithanbalagan@gmail.com>
2025-06-26 23:59:45 +0530

    Create main.py

---
+++ Calculator Project/main.py
@@ -0,0 +1,53 @@
+import art
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+# print(operations["*"](4, 8))
+
+
+def calculator():
+    print(art.logo)
+    a=1
+    num1 = float(input("What is the first number?: "))
+
+    while a==1:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            a=2
+            print("\n" * 20)
+            calculator()
+
+
+calculator()
